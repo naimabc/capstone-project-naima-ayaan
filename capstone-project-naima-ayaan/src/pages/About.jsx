16c330fdb41f0cbf8f642905c7df22b0808da5c0
naimabc <naimabc13@gmail.com>
2026-03-11 09:56:52 -0700

    my current work

--- capstone-project-naima-ayaan/src/pages/About.jsx
+++ capstone-project-naima-ayaan/src/pages/About.jsx
@@ -1,4 +1,4 @@
-import Header from "./components/Header.jsx";
+import Header from "../components/Header.jsx";
 function About() {
   return (
     <Header title='About Us' subtitle='Meet the team!'/>
