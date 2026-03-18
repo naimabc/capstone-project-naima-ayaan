34ec25b03b38b5eeb5683f4415bddcc44f63c31f
naimabc <naimabc13@gmail.com>
2026-03-18 10:03:32 -0700

    added stuff to profile page

--- capstone-project-naima-ayaan/src/pages/Home.jsx
+++ capstone-project-naima-ayaan/src/pages/Home.jsx
@@ -1,9 +1,11 @@
 import Header from "../components/Header.jsx";
 
+
 function Home() {
     return(
         <div>
             <Header title='Welcome!' subtitle='subtitle'/>
+            
         </div>
     );
 }
