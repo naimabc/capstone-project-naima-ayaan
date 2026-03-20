8750e1c15e6793fe74e731d7f2ae3f91c7b01b42
AyaanSL <ayaanshethlin@gmail.com>
2026-03-20 15:52:55 -0700

    log in page

--- capstone-project-naima-ayaan/src/pages/About.jsx
+++ capstone-project-naima-ayaan/src/pages/About.jsx
@@ -6,9 +6,9 @@ function About() {
   return (
     <div>
       <Header title='About Us' subtitle='Meet the team!'/>
+      <Text subtitle='Our Mission' text='mission here'/>
       <TeamProfile name='Naima' role='Code Manager' bio='bio here' img='https://emojiguide.org/images/emoji/c/8z8e4013re4yc.png'/> 
       <TeamProfile name='Ayaan' role='Project Manager' bio='bio here' img='https://emojiguide.org/images/emoji/c/8z8e4013re4yc.png'/>
-      <Text subtitle='Our Mission' text='mission here'/>
     </div>
 
 
