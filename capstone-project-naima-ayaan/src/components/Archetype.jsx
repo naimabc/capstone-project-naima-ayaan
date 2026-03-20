b0663da820ae92f965ee4823066584dac7d227c4
naimabc <naimabc13@gmail.com>
2026-03-20 09:27:02 -0700

    Update Archetype.jsx

--- capstone-project-naima-ayaan/src/components/Archetype.jsx
+++ capstone-project-naima-ayaan/src/components/Archetype.jsx
@@ -1,20 +1,16 @@
-function Archetype({ArchName, genres, info, image}) {
+
+import {useState} from "react";
+
+function Archetype({archName, genres, info, image}) {
+const [showMore, setShowMore] = useState(false);
     return (
-        <div class="border rounded-3xl p-10 m-20 object-center">  
-            <h2 class="text-2xl">{ArchName}:</h2>
+        <div class="text-center border rounded-xl p-10 m-20 max-w-xl mx-auto">
+            <h2 class="text-2xl">{archName}:</h2>
             <h5 class="text-lg">Genres: {genres}</h5>
             <br></br>
             <img src={image}></img>
             <br></br>
             <p >{info}</p>
-import {useState} from "react";
-
-function Archetype({type, description}) {
-const [showMore, setShowMore] = useState(false);
-    return (
-        <div class="text-center border rounded-xl p-10 m-20 max-w-xl mx-auto">
-            <h1 class="text-3xl">The {type}:</h1>
-            <p>{description}</p>
             <button
                 onClick={() => setShowMore(!showMore)} className="mt-4 text-sm hover:text-blue-700 font-semibold">
                 {showMore ? "▲ Show Less" : "▼ Show More"}
