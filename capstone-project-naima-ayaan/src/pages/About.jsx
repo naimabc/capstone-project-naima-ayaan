5c6151b519c86b595fd13823853d0ef917a0fe57
naimabc <naimabc13@gmail.com>
2026-03-25 10:08:21 -0700

    made quiz question component

--- capstone-project-naima-ayaan/src/pages/About.jsx
+++ capstone-project-naima-ayaan/src/pages/About.jsx
@@ -2,6 +2,7 @@ import Header from "../components/Header.jsx";
 import TeamProfile from "../components/TeamProfile.jsx";
 import Text from "../components/Text.jsx";
 
+
 function About() {
   return (
     <div>
@@ -9,6 +10,7 @@ function About() {
       <Text subtitle='Our Mission' text='mission here'/>
       <TeamProfile name='Naima' role='Code Manager' bio='bio here' img='https://emojiguide.org/images/emoji/c/8z8e4013re4yc.png'/> 
       <TeamProfile name='Ayaan' role='Project Manager' bio='bio here' img='https://emojiguide.org/images/emoji/c/8z8e4013re4yc.png'/>
+      
     </div>
 
 
