749d00c2ec1f89b91cd633a87fdbd3f088305e5b
naimabc <naimabc13@gmail.com>
2026-03-27 10:03:55 -0700

    finished making archetype components

---
+++ capstone-project-naima-ayaan/src/components/archetypes/Type2.jsx
@@ -0,0 +1,9 @@
+import Archetype from "../archetypes/Archetype";
+function Type2() {
+    return (
+        <div>  
+            <Archetype archName="Type 2" subtitle="short description" info="full description" image=""/>
+       </div>
+    );
+}
+export default Type2;
---
+++ capstone-project-naima-ayaan/src/components/archetypes/Type4.jsx
@@ -0,0 +1,9 @@
+import Archetype from "../archetypes/Archetype";
+function Type4() {
+    return (
+        <div>  
+            <Archetype archName="Type 4" subtitle="short description" info="full description" image=""/>
+       </div>
+    );
+}
+export default Type4;
