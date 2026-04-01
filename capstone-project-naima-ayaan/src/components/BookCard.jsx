a688a51f90229e058733e26fd5770a63ca9acfe9
AyaanSL <ayaanshethlin@gmail.com>
2026-04-01 10:25:31 -0700

    hello link

--- capstone-project-naima-ayaan/src/components/BookCard.jsx
+++ capstone-project-naima-ayaan/src/components/BookCard.jsx
@@ -1,10 +1,10 @@
 function BookCard({cover, title, author}) {
     return (
-        <div class="font-serif text-leaves border h-200 border-leaves rounded-3xl p-5 mb-10">  
+        <div class="font-serif text-leaves border h-200 w-170 border-leaves rounded-xl p-5 mb-10">  
             <img src={cover} className="object-contain"></img>
             <br></br>
-            <h2 class="text-xl">{title}</h2>
-            <h5 class="text-sm">{author}</h5>
+            <h2 class="text-xl text-leaves">{title}</h2>
+            <h5 class="text-sm text-leaves">{author}</h5>
        </div>
     );
 }
