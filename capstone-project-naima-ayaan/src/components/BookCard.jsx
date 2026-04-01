0a1e56af101b2f0da732d1870befaea926547523
AyaanSL <ayaanshethlin@gmail.com>
2026-04-01 10:36:32 -0700

    books preetty much done

--- capstone-project-naima-ayaan/src/components/BookCard.jsx
+++ capstone-project-naima-ayaan/src/components/BookCard.jsx
@@ -1,6 +1,6 @@
 function BookCard({cover, title, author}) {
     return (
-        <div class="font-serif text-leaves border h-200 w-170 border-leaves rounded-xl p-5 mb-10">  
+        <div class="font-serif text-leaves border h-200 w-170 border-leaves rounded-xl p-5 mb-10 ">  
             <img src={cover} className="object-contain"></img>
             <br></br>
             <h2 class="text-xl text-leaves">{title}</h2>
--- capstone-project-naima-ayaan/src/pages/Book.jsx
+++ capstone-project-naima-ayaan/src/pages/Book.jsx
@@ -9,7 +9,7 @@ function Book() {
             <Header title='Jane Eyre' subtitle='Charlotte Bronte'/>
             <div className="columns-3 flex gap-10 max-sm:columns-2">
 
-            <BookCard cover="https://i5.walmartimages.com/seo/Jane-Eyre-Masterpiece-Library-Edition-Hardcover-9781441342140_e9924ae6-71b6-4018-89b3-3a9590bb934f.b05ce944f40376224ecdd2a7b323db46.jpeg" class="w-1/2" title="" author=""/>
+            <BookCard cover="https://m.media-amazon.com/images/I/91mWGZ17QRL._AC_UF1000,1000_QL80_.jpg" title="The Memory Police" author="Yoko Ogawa"/>
             <Text subtitle='Synopsis' text='synopsis of book here blah blah blah poor orphen girl blah blah blah older dude who is secretly married'/>
             </div>
         </>
