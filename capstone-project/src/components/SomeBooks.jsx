427907e3d9c4762150817da8547a49854659205a
AyaanSL <ayaanshethlin@gmail.com>
2026-04-22 10:59:00 -0700

    trying to make live link work

--- capstone-project/src/components/SomeBooks.jsx
+++ capstone-project/src/components/SomeBooks.jsx
@@ -1,12 +1,13 @@
 
 import BookCardMany from "./BookCardMany.jsx"
+import { Link } from "react-router-dom";
 
 function SomeBooks() {
     return (
         <div class="font-serif text-leaves border rounded-xl p-10 m-10 columns-4 gap-10 max-sm:columns-2">
             
             <BookCardMany cover="https://m.media-amazon.com/images/I/91K8LzXdaYL._AC_UF1000,1000_QL80_.jpg" title="What it Means When a Man Falls From the Sky" author="Lesley Nneka Arimah"/>
-            <BookCardMany cover="https://prodimage.images-bn.com/pimages/9781101911815_p0_v2_s1200x630.jpg" title="The Memory Police" author="Yoko Ogawa"/>
+            <Link to="/Book"><BookCardMany cover="https://prodimage.images-bn.com/pimages/9781101911815_p0_v2_s1200x630.jpg" title="The Memory Police" author="Yoko Ogawa"/></Link>
             <BookCardMany cover="https://m.media-amazon.com/images/I/71Q0c9qf-1L._AC_UF1000,1000_QL80_.jpg" title="Slaughterhouse-Five" author="Kurt Vonnegut"/>
             <BookCardMany cover="https://m.media-amazon.com/images/I/71Rd-R7WcqL._UF1000,1000_QL80_.jpg" title="Under the Eye of the Big Bird" author="Hiromi Kawakami"/>
 
