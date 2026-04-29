400cc5a197ed6c25ccae443ccbcbfed7320cf9f5
naimabc <naimabc13@gmail.com>
2026-04-29 10:02:37 -0700

    added moew quesrions

--- capstone-project/src/pages/Quiz.jsx
+++ capstone-project/src/pages/Quiz.jsx
@@ -9,11 +9,11 @@ function Quiz() {
   return(
     <div>
       <Header title='Quiz' subtitle='What Archetype Are You?'/>
-      
-
       <QuizQuestion question='Which of the following stories sounds the most interesting?' option1='A courageous hero goes on a high-stakes mission to infiltrate a supervillain’s headquarters before the city is destroyed, culminating in a suspenseful final battle' option2='A poet struggles with finding purpose following the death of a loved one—told through a series of fragmented journal entries' option3='A woman forms a heartwarming friendship with a sick animal at her local zoo, nurturing it as it heals' option4='A man discovers his reality is not what it seems as he begins to realize he is stuck in a time loop'/>
-      <QuizQuestion question='When you pick up a book, what are you hoping to find inside its pages?' option1='High-stakes suspense—you want something entertaining that will keep you on your toes' option2='Philosophical and emotional depth—you want to explore the mysteries of the universe and the human experience' option3='Optimistic and uplifting stories—you want to escape from the stress of daily life' option4='Norm-defying narratives—you want something original that challenges how you see the world'/>
-      <QuizQuestion question='What is your preferred type of narrative voice?' option1='Event-focused and action-oriented—without wasting too much time on descriptions or characters’ thoughts and emotions' option2='Poetic, lyrical prose—you don’t mind abstract language or philosophical tangents' option3='Simple and easy to read—you want reading to be relaxing, and you don’t want to have to decipher the story' option4='Unreliable or unconventional narration—you want your perspective to be challenged'/>
+      <QuizQuestion question='Question' option1='Option 1' option2='Option 2' option3='Option 3' option4='Option4'/>
+      <QuizQuestion question='Question' option1='Option 1' option2='Option 2' option3='Option 3' option4='Option4'/>
+      <QuizQuestion question='Question' option1='Option 1' option2='Option 2' option3='Option 3' option4='Option4'/>
+      <QuizQuestion question='Question' option1='Option 1' option2='Option 2' option3='Option 3' option4='Option4'/>
       
       <Link to="/quizresults"><QuizButton title='See Results'/></Link>
     </div>
