551a4ef2a7f740eaf743429b1513a8c0ee5f1d89
AyaanSL <ayaanshethlin@gmail.com>
2026-05-01 11:33:19 -0700

    some books update

--- capstone-project/src/components/SomeBooks1.jsx
+++ capstone-project/src/components/SomeBooks1.jsx
@@ -6,9 +6,9 @@ function SomeBooks1() {
     return (
         <div class="font-serif text-leaves border rounded-xl p-10 m-10 columns-4 gap-10 max-sm:columns-2">
             
-            <Link to="/Books/eyesgod"><BookCardMany cover="https://m.media-amazon.com/images/I/71YvIyTP6fL._AC_UF1000,1000_QL80_.jpg" title="Their Eyes Were Watching God" author="Zora Neale Hurston"/></Link>
-            <Link to="/Books/mempol"><BookCardMany cover="https://prodimage.images-bn.com/pimages/9781101911815_p0_v2_s1200x630.jpg" title="The Memory Police" author="Yoko Ogawa"/></Link>
-            <Link to="/Books/slaught5"><BookCardMany cover="https://m.media-amazon.com/images/I/71Q0c9qf-1L._AC_UF1000,1000_QL80_.jpg" title="Slaughterhouse-Five" author="Kurt Vonnegut"/></Link>
+            <Link to="/Books/poppywar"><BookCardMany cover="https://m.media-amazon.com/images/I/71N+1tUQeoL._AC_UF1000,1000_QL80_.jpg" title="The Poppy War" author="R.F. Kuang"/></Link>
+            <Link to="/Books/endersgame"><BookCardMany cover="https://upload.wikimedia.org/wikipedia/en/e/e4/Ender%27s_game_cover_ISBN_0312932081.jpg" title="Ender's Game" author="Orson Scott Card"/></Link>
+            <Link to="/Books/silentpatient"><BookCardMany cover="https://m.media-amazon.com/images/I/91lslnZ-btL._AC_UF1000,1000_QL80_.jpg" title="The Silent Patient" author="Alex Michaelides"/></Link>
             <Link to="/Books/bigbird"><BookCardMany cover="https://m.media-amazon.com/images/I/71Rd-R7WcqL._UF1000,1000_QL80_.jpg" title="Under the Eye of the Big Bird" author="Hiromi Kawakami"/></Link>
 
         </div>
