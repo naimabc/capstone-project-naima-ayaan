d873035c1754d799f47c28bea415a9cae4fc27b5
naimabc <naimabc13@gmail.com>
2026-05-01 10:06:39 -0700

    started to figure out quiz

--- capstone-project/src/pages/QuizResults.jsx
+++ capstone-project/src/pages/QuizResults.jsx
@@ -1,9 +1,6 @@
 import Header from "../components/Header.jsx";
 import SomeBooks4 from "../components/SomeBooks4.jsx";
 import Subtitle from "../components/Subtitle.jsx";
-import Type1 from "../components/archetypes/Type1.jsx";
-import Type2 from "../components/archetypes/Type2.jsx";
-import Type3 from "../components/archetypes/Type3.jsx";
 import Type4 from "../components/archetypes/Type4.jsx";
 
 
