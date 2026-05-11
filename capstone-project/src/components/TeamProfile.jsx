02d53b811cb5d9d81ce229e1545e9fc6ef9f9356
AyaanSL <ayaanshethlin@gmail.com>
2026-05-10 23:29:39 -0700

    profile updates lol

--- capstone-project/src/components/TeamProfile.jsx
+++ capstone-project/src/components/TeamProfile.jsx
@@ -1,7 +1,7 @@
 function TeamProfile({name, role, bio, image}) {
     return (
         <div class="border text-leaves border-nature rounded-lg p-10 m-20 max-w-sm mx-auto font-serif">  
-            <h2 class="text-2xl">{name}:</h2>
+            <h2 class="text-2xl">{name}</h2>
             <h5 class="text-lg">Role: {role}</h5>
             <br></br>
             <img src={image} className="border-double inline-block h-100 rounded-full"></img>
